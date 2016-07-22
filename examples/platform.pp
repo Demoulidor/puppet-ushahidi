05273442f3a1ac2aefc7684214d7f8af0109b641
Guto Carvalho <gutocarvalho@instruct.com.br>
2016-07-22 11:11:02 -0300

    Inital commit

---
+++ examples/platform.pp
@@ -0,0 +1,1 @@
+include ushahidi::platform
---
+++ manifests/platform.pp
@@ -0,0 +1,101 @@
+class ushahidi::platform(
+  $mysql_root_pass      = 'strong@pass',
+  $mysql_ushahidi_user  = 'ushahidi_user',
+  $mysql_ushahidi_pass  = 'ushahidi_pass',
+  $mysql_ushahidi_name  = 'ushahidi',
+  $ushahidi_www_dir     = '/srv/ushahidi',
+  $ushahidi_git_source  = 'https://github.com/ushahidi/platform.git',
+  ) {
+
+  $php_packages = [ 'php5-gd', 'php5-imap', 'php5-mcrypt', 'php5-json' ]
+
+  include apt
+  include apt::update
+
+  class { 'apache':
+    mpm_module => 'prefork'
+  }
+
+  include apache::mod::php
+  include apache::mod::rewrite
+
+  apache::vhost { 'ushahidi.dev':
+    port          => '80',
+    docroot       => $ushahidi_www_dir,
+    docroot_owner => 'www-data',
+    docroot_group => 'www-data',
+    options       => ['Indexes','FollowSymLinks','MultiViews'],
+    override      => ['None'],
+  }
+
+  package { $php_packages:
+    ensure => installed,
+  }
+
+  package { 'git':
+    ensure => installed,
+  }
+
+  class { '::mysql::server':
+    root_password => $mysql_root_pass,
+  }
+
+  include mysql::bindings
+  include mysql::bindings::php
+
+  mysql::db { $mysql_ushahidi_name:
+    user     => $mysql_ushahidi_user,
+    password => $mysql_ushahidi_pass,
+    host     => $::hostname,
+    grant    => ['all'],
+  }
+
+  vcsrepo { $ushahidi_www_dir:
+    ensure   => present,
+    provider => 'git',
+    source   => $ushahidi_git_source,
+  }
+
+  file {  "${ushahidi_www_dir}/.env":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => epp('ushahidi/env.epp'),
+  }
+
+  file { "${ushahidi_www_dir}/.htaccess":
+    ensure  => present,
+    content => epp('ushahidi/htaccess.epp'),
+  }
+
+  file { "${ushahidi_www_dir}/application/logs":
+    mode  => '0775',
+    owner => 'www-data',
+    group => 'www-data',
+  }
+
+  file { "${ushahidi_www_dir}/application/cache":
+    mode  => '0775',
+    owner => 'www-data',
+    group => 'www-data',
+  }
+
+  file { "${ushahidi_www_dir}/application/media/uploads":
+    mode  => '0775',
+    owner => 'www-data',
+    group => 'www-data',
+  }
+
+  file { "${ushahidi_www_dir}/application/config/environments":
+    ensure => directory
+  }
+
+  file { "${ushahidi_www_dir}/application/config/environments/development":
+    ensure  => directory,
+    require => File["${ushahidi_www_dir}/application/config/environments"]
+  }
+
+  include composer
+
+}
